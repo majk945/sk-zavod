f03a7e8237bcf77d3b20fe051345e9b063bd4067
Majk <michalliptak40@gmail.com>
2026-02-25 14:33:35 +0100

    app init

---
+++ sk-zavod/src/app/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
